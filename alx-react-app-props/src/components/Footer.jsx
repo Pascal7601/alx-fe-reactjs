73d86e29500fe88672e2b8094e8ae7d7ef281d17
Pascal7601 <levispascal63@gmail.com>
2025-11-04 17:14:33 +0300

    feat: implemented a sample use context api

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+        <footer style={{ padding: '10px'}}>
+            <p style={{ textAlign: 'center' }}>© 2023 City Lovers</p>
+        </footer>
+    )
+}
+export default Footer
---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent() {
+    return (
+        <main style={{ padding: '20px'}}>
+            <p style={{ fontFamily: 'sans-serif'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    )
+}
+export default MainContent
