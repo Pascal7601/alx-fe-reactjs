01d39c7c48aabf83c6520016f302cffabbee563f
Pascal7601 <levispascal63@gmail.com>
2025-11-04 17:49:19 +0300

    changed bg of navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', margin: 'auto 0'}}>
+    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', backgroundColor: 'whitesmoke'}}>
         <Link to="/">Home</Link>
         <Link to="/about">About Us</Link>
         <Link to="/services">Services</Link>
