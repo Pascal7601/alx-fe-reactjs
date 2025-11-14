23898789060c528dd4a3a2e5f22b24aaabdb2b63
Pascal7601 <levispascal63@gmail.com>
2025-11-14 18:58:13 +0300

    fix: renamed setRcipe to setRecipes to satisfy the checker

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,5 +4,5 @@ export const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipe: (recipes) => set({ recipes }),
+  setRecipes: (recipes) => set({ recipes }),
 }));
