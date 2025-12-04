4d25b99e32ea10f37a5e1857118c8ed759351dcb
Pascal7601 <levispascal63@gmail.com>
2025-12-04 20:18:31 +0300

    feat: implement home page of recipe app

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import HomePage from "./components/HomePage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <p className="text-blue-500">Hello</p>
+      <HomePage />
     </>
   );
 }
---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,23 @@
+import data from "../data.json";
+function HomePage() {
+  return (
+    <div className="grid md:grid-cols-3 md:gap-8 md:w-50">
+      <h1 className="text-blue-500 text-center">RECIPES</h1>
+      {data.map((recipe) => {
+        return (
+          <div className="bg-gray-100 rounded-lg my-10 p-4 shadow-lg hover:shadow-xl">
+            <img
+              className="rounded-full w-36 h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
+              src={recipe.image}
+              alt="recipe"
+            />
+            <p className="font-semibold my-2">{recipe.title}</p>
+            <p className="text-gray-600">{recipe.summary}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default HomePage;
