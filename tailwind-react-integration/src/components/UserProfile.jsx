d87fb2f989bbc4313b2440e6c3333941ba24f66c
Pascal7601 <levispascal63@gmail.com>
2025-12-04 18:27:23 +0300

    feat: add shadwo to container

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-500 p-8 max-w-sm mx-auto my-20 rounded-lg">
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
         className="rounded-full w-36 h-36 mx-auto"
         src="https://images.pexels.com/photos/34943688/pexels-photo-34943688.jpeg?_gl=1*1htv11c*_ga*MTY4ODQ4MTkxMi4xNzM3Nzk5MjAz*_ga_8JE65Q40S6*czE3NjQ4NjE1MzAkbzkkZzAkdDE3NjQ4NjE1MzAkajYwJGwwJGgw"
