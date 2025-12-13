b1999adae9c95e6b10073e6722e4875f7579ac70
Pascal7601 <levispascal63@gmail.com>
2025-12-13 19:30:11 +0300

    fix: rename post to fetchposts

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
 
-const url = "https://jsonplaceholder.typicode.com/posts";
+const url = "https://jsonplaceholder.typicode.com/pos";
 
 function PostsComponent() {
   const { isLoading, data, isError, error } = useQuery({
-    queryKey: ["posts"],
-    queryFn: () => {
-      return fetch(url).then((data) => data.json());
+    queryKey: ["fetchPosts"],
+    queryFn: async () => {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Failed to fetch posts...");
+      }
+      return response.json();
     },
   });
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return <h2>Error fetching posts... {error.message}</h2>;
   }
   return (
     <div>
